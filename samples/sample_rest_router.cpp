0d31f376cd23719f4ef155343a07dbe4fd5cefc7
Thomas Gourgues <thomas@MacBook-Pro-de-Thomas.local>
2019-02-14 16:58:48 +0100

    Fixed events cmake target features for cmake version < 3.8

--- samples/sample_rest_router.cpp
+++ samples/sample_rest_router.cpp
@@ -34,9 +34,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <puffin/rest/router.hpp>
-
 int main(int argc, char* argv[]) {
-  
+
   return 0;
 }
